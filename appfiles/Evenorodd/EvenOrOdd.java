5e585b02d74a915b2841cd5f60a5ee7b22e08eaa
prapul <prapul.mannem@gmail.com>
2022-03-08 17:48:28 +0530

    all files commited

---
+++ appfiles/Evenorodd/EvenOrOdd.java
@@ -0,0 +1,22 @@
+package concatation;
+
+public class EvenOrOdd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n = 13;
+       if(n % 2 == 0)
+       {
+    	   System.out.println(n+"is even number");
+       }
+       else
+       {
+    	   System.out.println(n+"is odd number");
+       }
+       
+	}
+
+}
